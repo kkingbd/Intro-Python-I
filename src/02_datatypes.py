c209423c4b36641ab3c227409325bb02b1682cbf
Kawsar Hussen <kawsarhussen@gmail.com>
2019-05-28 15:46:51 -0400

    12 and 57 printed , datatype completed

--- src/02_datatypes.py
+++ src/02_datatypes.py
@@ -14,8 +14,12 @@ y = "7"
 # Write a print statement that combines x + y into the integer value 12
 
 # YOUR CODE HERE
-
+# print(x + int(y))
+addNum = x + int(y)
+print(addNum)
 
 # Write a print statement that combines x + y into the string value 57
 
 # YOUR CODE HERE
+addStrings = str(x) + y
+print(addStrings) 
